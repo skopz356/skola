14976c4aa53d71607e0b65f28f0b0ce7099dacc4
Tomáš Kubát <kubat.tomas@studenti.gymjil>
2018-09-25 12:19:58 +0200

    add form

--- test/admin.php
+++ test/admin.php
@@ -12,7 +12,7 @@ echo $head;
             <input type="password" name="password">
             <input type="submit" name="submit" value="Odeslat">
         </form>
-        <div class="notification">Uspesne prihlasen</div>
+        <div class="notification">Spatne heslo</div>
         <script>
         $('#form').submit(function(event){
             event.preventDefault();
@@ -23,7 +23,11 @@ echo $head;
             data: data
         });
         request.done(function(response){
-            location.reload();
+            if(response == "Succ"){
+                location.reload();
+            }else{
+                $('.notification').toggle();               
+            }
             
         });
 
@@ -47,29 +51,56 @@ echo $head;
                         echo "</form>";
                     }
                 }
+                
+
             }?>
+            <button class='pridat'>Pridat Clanek</button>
+            <form id='add-form'>
+                <textarea name="kategorie"></textarea>
+                <textarea name="nadpis"></textarea>
+                <textarea name="text"></textarea>
+                <input type="submit" value="Odeslat">
+            </form>
+
             <script>
-            $('#update-form').submit(function(event){
-            event.preventDefault();
-            data = $(this).serialize();
-            var request = $.ajax({
-            url: "update.php",
-            type: "POST",
-            data: data
-        });
-        request.done(function(response){
-            if(response == "Succ"){
-                location.reload();
-            }
-            else{
-                alert("Response")
-            }
-            
-            
-        });
-    });
+                $('#update-form').submit(function(event){
+                    event.preventDefault();
+                    data = $(this).serialize();
+                    var request = $.ajax({
+                    url: "update.php",
+                    type: "POST",
+                    data: data
+                });
+                    request.done(function(response){
+                        if(response == "Succ"){
+                            location.reload();
+                        }
+                        else{
+                            alert("Response")
+                        }
+                    });
+                });
+                $('.pridat').click(function(){
+                        $('#add-form').toggle();
 
-
+                });
+                $('#add-form').submit(function(event){
+                    event.preventDefault();
+                    data = $(this).serialize();
+                    var request = $.ajax({
+                    url: "add.php",
+                    type: "POST",
+                    data: data
+                });
+                    request.done(function(response){
+                        if(response == "Succ"){
+                            location.reload();
+                        }
+                        else{
+                            alert("Response")
+                        }
+                    });
+                });
             </script>
 
 
@@ -80,55 +111,7 @@ echo $head;
 
 
 ?>
-if(isset($_SESSION["login"])){
-            if($_SESSION["login"] == TRUE){
-                $sql = "SELECT id,kategorie, nadpis, text  FROM prispevky";
-                $result = $conn->query($sql);         
-                if ($result->num_rows > 0) {
-                    // output data of each row
-                    while($row = $result->fetch_assoc()) {
-                        echo "<form id='add-form' method='post'>";
-                        echo "<textarea name='kategorie'>".$row["kategorie"]."</textarea>";
-                        echo "<textarea name='nadpis'>".$row["nadpis"]."</textarea>";
-                        echo "<textarea name='text'>".$row["text"]."</textarea>";
-                        echo "<input type='hidden' name='id' value='".$row["id"]."'"." >";
-                        echo "<input type='submit'>";
-                        echo "</form>";
-                    }
-                }
-            }?>
-            <script>
-            $('#update-form').submit(function(event){
-            event.preventDefault();
-            data = $(this).serialize();
-            var request = $.ajax({
-            url: "update.php",
-            type: "POST",
-            data: data
-        });
-        request.done(function(response){
-            if(response == "Succ"){
-                location.reload();
-            }
-            else{
-                alert("Response")
-            }
-            
-            
-        });
-    });
 
-
-            </script>
-
-
-
-
-            <?php
-        }
-        
-
-?>
 
 </body>
 </html>
--- test/login.php
+++ test/login.php
@@ -4,7 +4,9 @@ session_start();
 if (isset($_POST["username"])){
     if(($_POST["username"] == "admin") && ($_POST["password"] == "heslo")){
         $_SESSION["login"] = TRUE;
-       
+        echo "Succ";
+    }else{
+        echo "Bad";   
     }
 }
 ?>
